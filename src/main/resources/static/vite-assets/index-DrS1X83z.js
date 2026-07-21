32c6ae9ef0ae2b195ee45c5b7a212df13a2a92b2
Tooltingsu <303443287@qq.com>
2026-07-22 02:24:10 +0800

    build: bundle kugou lyric source frontend

---
+++ src/main/resources/static/vite-assets/index-DrS1X83z.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-DNQMyGk8.js";var a=globalThis?.document?o.useLayoutEffect:o.useEffect;export{a as u};
---
+++ src/main/resources/static/vite-assets/src-UW24ZMRV-DRVp-Fh5.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-DtJ4Ehlu.js";import"./index-DNQMyGk8.js";var t=a;export{t as default};
